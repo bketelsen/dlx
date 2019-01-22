aff918b801664ec6f5b3820357c2f144cb9df7e8
Brian Ketelsen <mail@bjk.fyi>
2019-01-22 07:57:26 -0500

    fix gui profile for cgroups

--- templates/provision/slack.sh
+++ templates/provision/slack.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 set -e
 
+
+sudo apt update
+sudo apt install --yes gconf2 gconf-service libgtk2.0-0 libnotify4 libnss3 python gvfs-bin xdg-utils libxss1 libappindicator1 libsecret-1-0
 wget -O slack-desktop-3.3.3-amd64.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.3-amd64.deb
-sudo dpkg --install slack-desktop-3.2.1-amd64.deb
-rm, -f, slack-desktop-3.2.1-amd64.deb
-apt --yes --fix-broken install
+sudo dpkg --install slack-desktop-3.3.3-amd64.deb
+rm -f slack-desktop-3.3.3-amd64.deb
