421ee8d7f63962a56212140d2edbe9ceea49018f
Brian Ketelsen <mail@bjk.fyi>
2019-01-24 19:36:39 -0500

    fix template command

--- cmd/rmtemplate.go
+++ cmd/rmtemplate.go
@@ -31,7 +31,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("rm called")
+		fmt.Println("I'm afraid to implement this, because running containers depend on the images.  TBD")
 	},
 }
 
--- cmd/template.go
+++ cmd/template.go
@@ -15,8 +15,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -30,9 +28,6 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("template called")
-	},
 }
 
 func init() {
